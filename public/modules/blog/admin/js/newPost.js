0ceee962734deeb0e22f9b083910b86da3a76717
Andrew Cobby <andrew.cobby@gmail.com>
2011-02-20 12:45:06 +1000

    Blog admin static file

---
+++ public/modules/blog/admin/js/newPost.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    if(!Modernizr.input.placeholder){
+       // $$("label").show();
+    }
+    
+    $('title').keyup(function(event){
+	var slugize = function(string){
+	    string = jQuery.trim(string);
+	    
+	}
+    });
+    
+    
+});
